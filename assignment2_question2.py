30251891df154823f356fc5ce6f8a477c7dbf341
alsonjiang <alson820@gmail.com>
2023-08-29 23:17:45 +0800

    completed all qns

--- assignment2_question2.py
+++ assignment2_question2.py
@@ -1,13 +1,27 @@
 from turtle import *
 
 def draw_polygon(d, n):
+    if n >= 3:
+        internal_angle = ((n - 2) * 180) / n #check internal angles
+        external_angle = 180 - internal_angle #turtle takes angle from straight ahead
+        for i in range(n):
+            forward(d)
+            left(external_angle)
+    else:
+        pass
+
+
+draw_polygon(200,4)
+
+
+"""
+def draw_polygon(d, n):
     internal_angle = ((n - 2) * 180) // n #check internal angles
     external_angle = 180 - internal_angle #turtle takes angle from straight ahead
-    for i in range(n):
-        forward(d)
-        left(external_angle)
+    turtle_movement(d, n, external_angle)
 
-
-draw_polygon(200,6)
-
-
+def turtle_movement(distance, sides, angle):
+    for i in range(sides):
+        forward(distance)
+        left(angle)
+"""
