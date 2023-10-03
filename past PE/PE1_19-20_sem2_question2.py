24f77f7fc3d73dfcdba96bc0ef7cc28fc230dc50
alsonjiang <alson820@gmail.com>
2023-10-03 23:54:38 +0800

    updated

---
+++ past PE/PE1_19-20_sem2_question2.py
@@ -0,0 +1,13 @@
+
+def make_bouquet(shop:list, number: int) -> bool:
+    if shop[]
+
+
+
+
+
+
+
+
+flowers_r_us = [("R", 5, 3), ("R", 3, 2), ("W", 4, 3), ("W", 2, 2), ("P", 3, 4)]
+print(make_bouquet(flowers_r_us, 10))
