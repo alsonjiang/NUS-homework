362fd7b335016e641b36fe226fa3c0ae1b2906f4
alsonjiang <alson820@gmail.com>
2023-10-06 19:11:58 +0800

    lala

--- past PE/19-20_sem2/PE1_19-20_sem2_question1.py
+++ past PE/19-20_sem2/PE1_19-20_sem2_question1.py
@@ -4,18 +4,19 @@ def rotate(bouquet: tuple, step: int) -> tuple:
 
     return rotated
 
-#print(rotate(("R", "P", "W", "W", "P", "R", "R", "R"), 7))
+#print(rotate(("R", "P", "W", "W", "P", "R", "R", "R"), 3))
 
 def flower_I(bouquet: tuple, k: int) -> str:
     bouquet_lst = list(bouquet)
-    flowers_gifted = []
-    while bouquet_lst is not []:
-        flowers_gifted.append(bouquet_lst[k])
-        bouquet_lst.remove(bouquet_lst[k])
-        bouquet_lst = rotate(bouquet_lst, k)
+    flowers_gifted = bouquet_lst[k-1] #first rose, kth from start
 
+    for i in range(len(bouquet_lst)):
+        bouquet_lst = rotate(bouquet_lst, k-1)
+        print(bouquet_lst)
+        flowers_gifted += bouquet_lst[2]
+    
     return flowers_gifted
-
+    
 print(flower_I(("R", "P", "W", "W", "P", "R", "R", "R"), 3))
 
 
